d602a38a596695db2c3de034e60acb2ca5657610
Zachary Windom <zww4855@gmail.com>
2022-10-18 18:14:36 -0400

    added beginnings of brueckner logic

--- brueckner/brueckner.py
+++ brueckner/brueckner.py
@@ -8,7 +8,8 @@ To avoid recomputing AO to MO integral transformation, integrals for CCSD,
 CCSD(T), CCSD lambda equation etc can be reused.
 '''
 
-import numpy 
+import numpy
+import numpy as np 
 from pyscf import gto, scf, cc
 
 mol = gto.M(verbose = 4,
@@ -27,6 +28,7 @@ coeff=mf.mo_coeff
 mo_occ=mf.mo_occ
 
 print('mo_occ:',mo_occ)
+print('coeff shape: ', coeff.shape)
 dm2=mf.make_rdm1(coeff,mo_occ)
 print(dm1==dm2)
 
@@ -35,13 +37,13 @@ print(coeff[:,mo_occ>0])
 #mf.init_guess=dm2
 #mf.kernel(max_cycle=0)
 
-#mycc = cc.CCSD(mf)
-#mycc.kernel()
+mycc = cc.CCSD(mf)
+mycc.kernel()
 
-#t1=mycc.t1
-#print('t1:',t1)
+t1=mycc.t1
+print('t1:',t1,np.linalg.norm(t1),t1.shape)
 #print('mo coeff',coeff)
-#nocc,nvirt=t1.shape
+nocc,nvirt=t1.shape
 #print('nocc:',nocc)
 #print('shape of coeff:', coeff.shape)
 
@@ -49,13 +51,37 @@ print(coeff[:,mo_occ>0])
 
 
 
-def run_brueckner(mf,t1,coeff,nocc):
-    dm_init_guess=coeff[:,:nocc]@coeff[:,:nocc].T
-    mf=scf.RHF(mol)#.run(dm0=dm_init_guess,max_cycle=0)
+def run_brueckner(mf,t1,coeff,nocc,mo_occ,t1TOL=10E-4):
+    t1norm=np.linalg.norm(t1)
+    t1matShape=np.shape(coeff)
+#    while t1norm > t1TOL:
+    for i in range(1):
+        print('norm of t1:',t1norm)
+        newC=coeff
+        t1mat=np.zeros(t1matShape)
+        print('t1mat:',t1mat.shape,t1.shape,nocc,mo_occ)
+        t1mat[:mo_occ,mo_occ:]=t1
+        t1mat[mo_occ:,:mo_occ]=t1.T
+        newC=coeff+coeff@t1mat
+        print('newC:',newC)
+        mf=scf.RHF(mol)
+        dm1=mf.make_rdm1(newC,nocc)
+        mf.init_guess=dm1
+        mf.run(max_cycle=0)
+       
+
+        mycc=cc.CCSD(mf)
+        mycc.kernel()
+        t1=mycc.t1
+        t1norm=np.linalg.norm(t1)
+        print('final t1norm:',t1norm)
+      
+    #dm_init_guess=coeff[:,:nocc]@coeff[:,:nocc].T
+    #mf=scf.RHF(mol)#.run(dm0=dm_init_guess,max_cycle=0)
     #dm_init_guess=#mf.make_rdm1(coeff,nocc)
-    mf.init_guess=dm_init_guess
-    mf.run(max_cycle=0)
+    #mf.init_guess=dm_init_guess
+    #mf.run(max_cycle=0)
     #mf.kernel(dm0=dm_init_guess,max_cycle=0)
 
-#run_brueckner(mf,t1,coeff,nocc)
+run_brueckner(mf,t1,coeff,mo_occ,nocc)
     
