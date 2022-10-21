b28c99e21495a9a833e2dd46f399550e3b5e60e4
Zachary Windom <zww4855@gmail.com>
2022-10-21 16:21:25 -0400

    finished adding brueckner logic; checking phase

--- brueckner/brueckner.py
+++ brueckner/brueckner.py
@@ -18,34 +18,17 @@ mol = gto.M(verbose = 4,
 
 
 mf=scf.RHF(mol)
-mf.chkfile='coeff'
 mf.kernel()
 dm1=mf.make_rdm1()
-print('converged DM',dm1)
-#print(scf.hf.energy_elec(mf, dm1))
-#mf = scf.RHF(mol).run()
 coeff=mf.mo_coeff
 mo_occ=mf.mo_occ
-
-print('mo_occ:',mo_occ)
-print('coeff shape: ', coeff.shape)
-dm2=mf.make_rdm1(coeff,mo_occ)
-print(dm1==dm2)
-
-print(coeff[:,mo_occ>0])
-
-#mf.init_guess=dm2
-#mf.kernel(max_cycle=0)
 
 mycc = cc.CCSD(mf)
 mycc.kernel()
 
 t1=mycc.t1
-print('t1:',t1,np.linalg.norm(t1),t1.shape)
-#print('mo coeff',coeff)
+print('initial t1:',t1)
 nocc,nvirt=t1.shape
-#print('nocc:',nocc)
-#print('shape of coeff:', coeff.shape)
 
 
 
@@ -55,17 +38,18 @@ def run_brueckner(mf,t1,coeff,nocc,mo_occ,t1TOL=10E-4):
     t1norm=np.linalg.norm(t1)
     t1matShape=np.shape(coeff)
 #    while t1norm > t1TOL:
-    for i in range(1):
+    for i in range(35):
         print('norm of t1:',t1norm)
         newC=coeff
         t1mat=np.zeros(t1matShape)
         print('t1mat:',t1mat.shape,t1.shape,nocc,mo_occ)
-        t1mat[:mo_occ,mo_occ:]=t1
-        t1mat[mo_occ:,:mo_occ]=t1.T
+        t1mat[:nocc,nocc:]=-1.0*t1
+        t1mat[nocc:,:nocc]=1.0*t1.T
         newC=coeff+coeff@t1mat
+        newC,r=np.linalg.qr(newC)
         print('newC:',newC)
         mf=scf.RHF(mol)
-        dm1=mf.make_rdm1(newC,nocc)
+        dm1=mf.make_rdm1(newC,mo_occ)
         mf.init_guess=dm1
         mf.run(max_cycle=0)
        
@@ -73,15 +57,28 @@ def run_brueckner(mf,t1,coeff,nocc,mo_occ,t1TOL=10E-4):
         mycc=cc.CCSD(mf)
         mycc.kernel()
         t1=mycc.t1
+        print('t1:',t1)
+        print('max of t1: ',np.max(t1))
         t1norm=np.linalg.norm(t1)
-        print('final t1norm:',t1norm)
-      
-    #dm_init_guess=coeff[:,:nocc]@coeff[:,:nocc].T
-    #mf=scf.RHF(mol)#.run(dm0=dm_init_guess,max_cycle=0)
-    #dm_init_guess=#mf.make_rdm1(coeff,nocc)
-    #mf.init_guess=dm_init_guess
-    #mf.run(max_cycle=0)
-    #mf.kernel(dm0=dm_init_guess,max_cycle=0)
+        print('****final t1norm:',t1norm)
 
-run_brueckner(mf,t1,coeff,mo_occ,nocc)
-    
+    print('new C: ', newC)   
+    return mf,dm1
+   
+
+mf,dm1=run_brueckner(mf,t1,coeff,nocc,mo_occ)
+mf.init_guess=dm1
+mf.run(max_cycle=0)   
+
+
+mycc=cc.CCSD(mf)
+old_update_amps = mycc.update_amps
+def update_amps(t1, t2, eris):
+    t1, t2 = old_update_amps(t1, t2, eris)
+    print('ccsd new t1', t1)
+    return t1, t2
+mycc.update_amps = update_amps
+mycc.kernel()
+
+print('CCSD correlation energy', mycc.e_corr)
+ 
