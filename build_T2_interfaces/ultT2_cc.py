60ef0eaef6cf26aadc1cca2fd06901917c280509
Zachary Windom <zww4855@gmail.com>
2022-11-02 16:21:13 -0400

    Added the beginning logic for ult T2. Only CCD so far.

---
+++ build_T2_interfaces/ultT2_cc.py
@@ -0,0 +1,125 @@
+import numpy as np
+from numpy import einsum
+
+def ccsd_energy(t1, t2, f, g, o, v):
+    """
+    < 0 | e(-T) H e(T) | 0> :
+
+    :param f:
+    :param g:
+    :param t1:
+    :param t2:
+    :param o:
+    :param v:
+    :return:
+    """
+
+    energy= 1.000000000000000 * einsum('ii', f[o, o])
+    #    -0.5000 <j,i||j,i>
+    energy += -0.5 * einsum('jiji', g[o, o, o, o])
+
+    #     0.2500 <j,i||a,b>*t2(a,b,j,i)
+    energy += 0.25 * einsum('jiab,abji', g[o, o, v, v], t2)
+
+
+    return energy
+
+def doubles_residual(t1, t2, f, g, o, v):
+    """
+     < 0 | m* n* f e e(-T) H e(T) | 0>
+
+    :param f:
+    :param g:
+    :param t1:
+    :param t2:
+    :param o:
+    :param v:
+    :return:
+    """
+
+
+#     -1.0000 P(i,j)f(k,j)*t2(a,b,i,k)
+    contracted_intermediate = -1.000000000000000 * einsum('kj,abik->abij', f[o, o], t2)
+    doubles_res =  1.00000 * contracted_intermediate + -1.00000 * einsum('abij->abji', contracted_intermediate)
+
+#      1.0000 P(a,b)f(a,c)*t2(c,b,i,j)
+    contracted_intermediate =  1.000000000000000 * einsum('ac,cbij->abij', f[v, v], t2)
+    doubles_res +=  1.00000 * contracted_intermediate + -1.00000 * einsum('abij->baij', contracted_intermediate)
+
+#      1.0000 <a,b||i,j>
+    doubles_res +=  1.000000000000000 * einsum('abij->abij', g[v, v, o, o])
+
+#      0.5000 <l,k||i,j>*t2(a,b,l,k)
+    doubles_res +=  0.500000000000000 * einsum('lkij,ablk->abij', g[o, o, o, o], t2)
+
+#      1.0000 P(i,j)*P(a,b)<k,a||c,j>*t2(c,b,i,k)
+    contracted_intermediate =  1.000000000000000 * einsum('kacj,cbik->abij', g[o, v, v, o], t2)
+    doubles_res +=  1.00000 * contracted_intermediate + -1.00000 * einsum('abij->abji', contracted_intermediate)  + -1.00000 * einsum('abij->baij', contracted_intermediate)  +  1.00000 * einsum('abij->baji', contracted_intermediate)
+
+#      0.5000 <a,b||c,d>*t2(c,d,i,j)
+    doubles_res +=  0.500000000000000 * einsum('abcd,cdij->abij', g[v, v, v, v], t2)
+
+#     -0.5000 P(i,j)<l,k||c,d>*t2(c,d,j,k)*t2(a,b,i,l)
+    contracted_intermediate = -0.500000000000000 * einsum('lkcd,cdjk,abil->abij', g[o, o, v, v], t2, t2, optimize=['einsum_path', (0, 1), (0, 1)])
+    doubles_res +=  1.00000 * contracted_intermediate + -1.00000 * einsum('abij->abji', contracted_intermediate)
+
+#      0.2500 <l,k||c,d>*t2(c,d,i,j)*t2(a,b,l,k)
+    doubles_res +=  0.250000000000000 * einsum('lkcd,cdij,ablk->abij', g[o, o, v, v], t2, t2, optimize=['einsum_path', (0, 1), (0, 1)])
+
+#     -0.5000 <l,k||c,d>*t2(c,a,l,k)*t2(d,b,i,j)
+    doubles_res += -0.500000000000000 * einsum('lkcd,calk,dbij->abij', g[o, o, v, v], t2, t2, optimize=['einsum_path', (0, 1), (0, 1)])
+
+#      1.0000 P(i,j)<l,k||c,d>*t2(c,a,j,k)*t2(d,b,i,l)
+    contracted_intermediate =  1.000000000000000 * einsum('lkcd,cajk,dbil->abij', g[o, o, v, v], t2, t2, optimize=['einsum_path', (0, 1), (0, 1)])
+    doubles_res +=  1.00000 * contracted_intermediate + -1.00000 * einsum('abij->abji', contracted_intermediate)
+
+#     -0.5000 <l,k||c,d>*t2(c,a,i,j)*t2(d,b,l,k)
+    doubles_res += -0.500000000000000 * einsum('lkcd,caij,dblk->abij', g[o, o, v, v], t2, t2, optimize=['einsum_path', (0, 2), (0, 1)])
+    return doubles_res
+
+
+def CCDkernel(t1, t2, fock, g, o, v, e_ai, e_abij, max_iter=100, stopping_eps=1.0E-8,
+           diis_size=None, diis_start_cycle=4):
+
+    # initialize diis if diis_size is not None
+    # else normal scf iterate
+    if diis_size is not None:
+        from diis import DIIS
+        diis_update = DIIS(diis_size, start_iter=diis_start_cycle)
+        t1_dim = t1.size
+        old_vec = np.hstack((t1.flatten(), t2.flatten()))
+
+    fock_e_ai = np.reciprocal(e_ai)
+    fock_e_abij = np.reciprocal(e_abij)
+    old_energy = ccsd_energy(t1, t2, fock, g, o, v)
+    print('old_E:', old_energy)
+    for idx in range(max_iter):
+
+        doubles_res = doubles_residual(t1, t2, fock, g, o, v) + fock_e_abij * t2
+
+        new_doubles = doubles_res * e_abij
+
+        # diis update
+        if diis_size is not None:
+            vectorized_iterate = np.hstack(
+                (new_singles.flatten(), new_doubles.flatten()))
+            error_vec = old_vec - vectorized_iterate
+            new_vectorized_iterate = diis_update.compute_new_vec(vectorized_iterate,
+                                                                 error_vec)
+            new_singles = new_vectorized_iterate[:t1_dim].reshape(t1.shape)
+            new_doubles = new_vectorized_iterate[t1_dim:].reshape(t2.shape)
+            old_vec = new_vectorized_iterate
+
+        current_energy = ccsd_energy(0, new_doubles, fock, g, o, v)
+        print('Current E:', current_energy)
+        delta_e = np.abs(old_energy - current_energy)
+
+        if delta_e < stopping_eps:
+            return  new_doubles
+        else:
+            t2 = new_doubles
+            old_energy = current_energy
+            print("\tIteration {: 5d}\t{: 5.15f}\t{: 5.15f}".format(idx, old_energy, delta_e))
+    else:
+        print("Did not converge")
+        return  new_doubles
